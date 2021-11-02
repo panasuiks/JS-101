50905228639428fa47f474af72f16b819a48682e
Stephen Panasuik <panasuiks@gmail.com>
2021-11-02 16:14:15 -0400

    11/2 afternoon commit

--- lesson2/calculator.js
+++ lesson2/calculator.js
@@ -23,67 +23,98 @@ Return number
 C:
 */
 
+const MESSAGES = require('./calculator_messages.json');
+
 const readline = require('readline-sync');
+let continueRunning = false;
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function invalidNumber(number) {
-  return ((number.trim() === '') || Number.isNaN(Number(number)));
+function message(title, lang = 'en') {
+  return MESSAGES[lang][title];
 }
 
-prompt('Welcome to calculator!');
+function calculator() {
 
-prompt('What\'s the first number?');
-let number1 = readline.question();
+  function invalidNumber(number) {
+    return ((number.trim() === '') || Number.isNaN(Number(number)));
+  }
 
-while (invalidNumber(number1)) {
-  prompt(`Hm... that doesn't look like a valid number.  Please input a valid number.`);
-  number1 = readline.question();
+  prompt(message('welcome', 'fr'));
+
+  prompt(message('requestFirstNumber'));
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    prompt(message('invalidNumber'));
+    number1 = readline.question();
+  }
+
+  prompt(message('requestSecondNumber'));
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    prompt(message('invalidNumber'));
+    number2 = readline.question();
+  }
+
+  prompt(message('requestOperation'));
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(message('invalidOperation'));
+    operation = readline.question();
+  }
+
+  let output;
+
+  switch (operation) {
+    case '1': //addition
+      output = Number(number1) + Number(number2);
+      break;
+    case '2': //subtraction
+      output = Number(number1) - Number(number2);
+      break;
+    case '3': //multiplication
+      output = Number(number1) * Number(number2);
+      break;
+    case '4': //division
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  /* This code reduces the output to two decimal places
+  max but will retain 0 decimal places for integer.*/
+  let decimalPlaces = 2;
+  function roundAndDecimal(inpNum, decimals) {
+    return Math.round(inpNum * (10 ** decimals)) / (10 ** decimals);
+  }
+
+  output = roundAndDecimal(output, decimalPlaces);
+
+
+  prompt(`${message('result')}${output}.\n`);
+  prompt(message('requestRetry'));
+  let retry = readline.question();
+
+  while ((retry.toLowerCase() !== 'yes') && (retry.toLowerCase() !== 'no')) {
+    prompt(message('invalidRetryEntry'));
+    retry = readline.question()
+  }
+
+  continueRunning = false
+  if (retry.toLowerCase() === 'yes') {
+    continueRunning = true
+  }
+
+
 }
 
-prompt('What\'s the second number? ');
-let number2 = readline.question();
+do {
+  calculator()
+} while (continueRunning === true)
 
-while (invalidNumber(number2)) {
-  prompt(`Hm... that doesn't look like a valid number.  Please input a valid number.`);
-  number2 = readline.question();
-}
+prompt(message('thanks'));
 
-prompt('What operation would you like to perform? \n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operation = readline.question();
-
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt(`Hm... Must choose 1, 2, 3, or 4.\n1) Add 2) Subtract 3) Multiply 4) Divide`);
-  operation = readline.question();
-}
-
-let output;
-
-switch (operation) {
-  case '1': //addition
-    output = Number(number1) + Number(number2);
-    break;
-  case '2': //subtraction
-    output = Number(number1) - Number(number2);
-    break;
-  case '3': //multiplication
-    output = Number(number1) * Number(number2);
-    break;
-  case '4': //division
-    output = Number(number1) / Number(number2);
-    break;
-}
-
-/* This code reduces the output to two decimal places
-max but will retain 0 decimal places for integer.*/
-let decimalPlaces = 2;
-function roundAndDecimal(inpNum, decimals) {
-  return Math.round(inpNum * (10 ** decimals)) / (10 ** decimals);
-}
-
-output = roundAndDecimal(output, decimalPlaces);
-
-
-prompt(`The result is: ${output}`);
