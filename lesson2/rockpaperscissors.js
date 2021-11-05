59f8ddc2fb2ed3be99895c8eda7cd7ee5e5f8b82
Steve <panasuiks@gmail.com>
2021-11-05 14:04:51 -0400

    11/5 mid day

--- lesson2/rockpaperscissors.js
+++ lesson2/rockpaperscissors.js
@@ -2,6 +2,7 @@
 
 const rlsync = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const WINS_FOR_WIN = 3;
 
 function prompt(message) {
   console.log('=>' + message);
@@ -29,44 +30,61 @@ function convertToValidInput(inpString) {
   let indexIterator = 0
   let matchedEntries = []
   let matchCounter = 0
-  let matchEntry = ''
-  let currentSearchArray = [VALID_CHOICES];
-  let result
-  while (matchCounter !== 1) {
+  let currentSearchArray = copyArrayValues(VALID_CHOICES);
+  let match = '';
+  while (indexIterator < inpString.length) {
     matchCounter = 0
-    currentSearchArray = matchedEntries
     matchedEntries = []
     for (index in currentSearchArray) {
       if (currentSearchArray[index][indexIterator] === inpString[indexIterator]) {
         matchCounter += 1;
-        matchIndex = currentSearchArray[index]
-        matchedEntries.push(VALID_CHOICES)
+        matchedEntries.push(currentSearchArray[index]);
       }
     }
+    if (matchCounter === 0) { break }
+    if (matchCounter === 1) { match = matchedEntries[0]; break }
+    currentSearchArray = copyArrayValues(matchedEntries);
+    indexIterator += 1;
   }
+  return (startMatchString(inpString, match)) ? match : undefined
+}
+
+function startMatchString(start, string) {
+  let match = true;
+  for (index in start) {
+    if (start[index] !== string[index]) {match = false; break }
+  }
+  return match
 }
 
 function copyArrayValues(inpArray) {
-  let returnArray = []
+  let returnArray = [];
   for (entry of inpArray) {
-    
+    returnArray.push(entry);
   }
+  return returnArray;
+}
+
+function maxStringLengthInArray(inpArray) {
+  let maxLength = 0
+  for (string of inpArray) {
+    if (string.length > maxLength) { maxLength = string.length }
+  }
+  return maxLength
 }
 
 
 
 
 console.clear()
-
+let userScore = 0
+let compScore = 0
 while (true) {
-
-  //prompt()//score
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let userChoice = rlsync.question();
-
+  let userChoice = convertToValidInput(rlsync.question());
   while (!VALID_CHOICES.includes(userChoice)) {
     prompt(`That\'s not a valid choice. Please choose one: ${VALID_CHOICES.join(', ')}`);
-    userChoice = rlsync.question();
+    userChoice = convertToValidInput(rlsync.question());
   }
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
@@ -77,8 +95,10 @@ while (true) {
   let winner = determineWinner(userChoice, computerChoice);
   if (winner === 1) {
     prompt('You win! Congratulations!');
+    userScore += 1;
   } else if (winner === 2) {
     prompt('Computer wins!');
+    compScore += 1;
   } else {
     prompt('It\'s a tie');
   }
@@ -92,4 +112,7 @@ while (true) {
 
   if (answer[0] === 'n') break;
   console.clear()
+
+  prompt(`The current score is user: ${userScore} | computer: ${compScore}\n`);
+
 }
