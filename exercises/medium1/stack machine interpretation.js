271bde534884dfa227b3277c027e6f58994bdc6f
Stephen Panasuik <panasuiks@gmail.com>
2021-11-29 14:39:43 -0500

    Nov29 Mid Day

--- exercises/medium1/stack machine interpretation.js
+++ exercises/medium1/stack machine interpretation.js
@@ -25,32 +25,55 @@ C:
 let stack = {
   stack: [],
   register: 0,
+  error: false,
+
+  'emptyStackError': 'The stack is empty.',
+
+  isStackEmpty() {
+    if (this.stack.length === 0) {
+      this.error = this.emptyStackError
+    }
+  },
+
+  getAndClearError() {
+    tempError = this.error;
+    this.error = false;
+    return tempError;
+  },
+
+  logEmptyStack() { console.log(emptyStackError) },
 
   PUSH() {
     this.stack.push(this.register);
   },
 
   ADD() {
+    this.isStackEmpty();
     this.register = this.stack.pop() + this.register;
   },
 
-  SUB() { 
+  SUB() {
+    this.isStackEmpty();
     this.register = this.register - this.stack.pop();
   },
 
   MULT() {
+    this.isStackEmpty();
     this.register = this.stack.pop() * this.register;
   },
 
   DIV() {
+    this.isStackEmpty();
     this.register = parseInt(this.register / this.stack.pop());
   },
 
-  REMAINDER() { 
+  REMAINDER() {
+    this.isStackEmpty();
     this.register = parseInt(this.register % this.stack.pop());
   },
 
   POP() {
+    this.isStackEmpty();
     this.register = this.stack.pop();
   },
 
@@ -66,13 +89,22 @@ let stack = {
 function minilang(inpString) {
   inpArray = inpString.split(' ');
   for (i = 0; i < inpArray.length; i += 1) {
-    let currentCommand = inpArray[i];
+    let currentCommand = inpArray[i].toUpperCase();
     if (isStringNumber(currentCommand)) {
       stack.NUMBERINPUT(currentCommand);
     } else {
-      stack[currentCommand]();
+      if (stack.hasOwnProperty(currentCommand)) {
+        stack[currentCommand]();
+        if (stack.error !== false) {
+          let error = stack.getAndClearError();
+          console.log(error);
+          return error;
+        }
+      } else {
+        console.log(`${currentCommand} is not a valid command`);
+      }
     }
-  } 
+  }
 }
 
 function isStringNumber(inpString) {
---
+++ exercises/medium1/word to digit.js
@@ -0,0 +1,59 @@
+/*
+P:
+Input: 
+  string
+Output:
+ string with number words converted to numbers
+ 
+
+
+D: 
+inp as string
+convert to array
+process as each 
+return as string
+
+A:
+set string
+set array
+for each
+  if object has key
+    convert
+  else
+    nothing
+join
+return
+
+C:
+*/
+
+let wordObject = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  zero: 0,
+}
+
+const wordToDigit = inpString => {
+  for (key in wordObject) {
+    inpString.replace(key, String(wordObject[key]));
+  }
+  return inpString;
+}
+
+const wordToDigit = inpString => {
+  let inpArray = inpString.split(' ',);
+  for (key in inpArray) {
+    let word = inpArray[key];
+    if (wordObject.hasOwnProperty(word)) {
+      inpArray[key] = wordObject[word]
+    }
+  }
+  return inpArray.join(' ');
+}
